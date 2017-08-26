a9fa9e915362b2e4904a86e597dd30c347962656
artefact <artefactgo@gmail.com>
2017-08-26 23:03:16 +0900

    サーバーからの受信データクラスをクラス化

---
+++ FrameworkNetwork/Scripts/Cores/AResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace com.Artefact.FrameworkNetwork.Cores
+{
+	/// <summary>
+	/// サーバーからの受信データクラスの抽象クラス
+	/// 
+	/// 全ての通信で、このクラスを継承したクラスを戻り値として指定してください。
+	/// </summary>
+	public abstract class AResponse
+	{
+		public static readonly string KeyResponseCode = "responceCode";
+
+		public static readonly string KeyData = "data";
+
+		public abstract void TryParse(JObject obj);
+
+		public int ResponseCode { get; protected set; }
+	}
+}
