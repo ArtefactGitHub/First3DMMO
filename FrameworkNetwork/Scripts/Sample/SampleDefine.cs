c2b1026369ee6042ae0b712b0876d2bbda392fff
artefact <artefactgo@gmail.com>
2017-08-26 23:04:33 +0900

    【サンプル】受信データクラスの修正

--- FrameworkNetwork/Scripts/Sample/SampleDefine.cs
+++ FrameworkNetwork/Scripts/Sample/SampleDefine.cs
@@ -1,7 +1,27 @@
-﻿namespace com.Artefact.FrameworkNetwork.Samples
+﻿using com.Artefact.FrameworkNetwork.Cores;
+using Newtonsoft.Json.Linq;
+
+namespace com.Artefact.FrameworkNetwork.Samples
 {
 	public static class SampleDefine
 	{
 		public static readonly string EndPoint = "ws://my-nodejs-app-artefactcloud.c9users.io/";
+	}
+
+	public class Response : AResponse
+	{
+		public string UserName { get; private set; }
+
+		public override void TryParse(JObject obj)
+		{
+			ResponseCode = obj[KeyResponseCode].ToObject<int>();
+
+			UserName = obj[KeyData]["userName"].ToObject<string>();
+		}
+
+		public override string ToString()
+		{
+			return string.Format("Response\n ResponseCode={0} UserName={1}", ResponseCode, UserName);
+		}
 	}
 }
