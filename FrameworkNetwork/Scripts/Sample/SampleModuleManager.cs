1aff1dd6bffcb764e09f7277d4574cb9903c416e
artefact <artefactgo@gmail.com>
2017-08-26 18:28:36 +0900

    【サンプル】モジュール管理クラスの追加

---
+++ FrameworkNetwork/Scripts/Sample/SampleModuleManager.cs
@@ -0,0 +1,32 @@
+﻿using com.Artefact.FrameworkNetwork.Cores;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace com.Artefact.FrameworkNetwork.Samples
+{
+	public class SampleModuleManager
+	{
+		#region singleton
+		private static SampleModuleManager _Instance = null;
+
+		public static SampleModuleManager Instance
+		{
+			get
+			{
+				if(_Instance == null)
+				{
+					_Instance = new SampleModuleManager();
+				}
+				return _Instance;
+			}
+		}
+
+		private SampleModuleManager()
+		{
+			Module = new SampleModule();
+		}
+		#endregion
+
+		public SampleModule Module { get; private set; }
+	}
+}
