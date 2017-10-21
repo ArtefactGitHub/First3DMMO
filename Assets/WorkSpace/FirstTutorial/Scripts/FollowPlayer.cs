4e3b6ae8d3cb416a472b00033018d47c5813c4c6
artefact <artefactgo@gmail.com>
2017-10-21 13:04:22 +0900

    公式チュートリアルの実装追加 　・https://github.com/unity3d-jp/FirstTutorial/wiki/3.%E3%82%AB%E3%83%A1%E3%83%A9%E3%82%92%E5%8B%95%E3%81%8B%E3%81%99

---
+++ Assets/WorkSpace/FirstTutorial/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	[SerializeField]
+	private Transform m_Target = null;
+
+	private Vector3 m_Offset = Vector3.zero;
+
+	private void Start()
+	{
+		m_Offset = GetComponent<Transform>().position - m_Target.position;
+	}
+
+	private void Update()
+	{
+		GetComponent<Transform>().position = m_Target.position + m_Offset;
+	}
+}
---
+++ Assets/WorkSpace/FirstTutorial/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float m_Speed = 10.0f;
+
+	private void FixedUpdate()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		rigidbody.AddForce(x * m_Speed, 0, z * m_Speed);
+	}
+}
