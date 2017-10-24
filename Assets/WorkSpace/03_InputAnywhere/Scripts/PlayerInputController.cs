0eb22de5cc6a2b9ac62791d743c64ad8852699f5
artefact <artefactgo@gmail.com>
2017-10-25 00:18:35 +0900

    不要処理の削除

--- Assets/WorkSpace/03_InputAnywhere/Scripts/PlayerInputController.cs
+++ Assets/WorkSpace/03_InputAnywhere/Scripts/PlayerInputController.cs
@@ -1,35 +1,15 @@
-﻿using UniRx;
-using UniRx.Triggers;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 namespace com.Artefact.First3DMMO.WorkSpace.InputStickAnywhere
 {
 	public class PlayerInputController : MonoBehaviour
 	{
-		public bool IsBoost { get; private set; }
-
 		[SerializeField]
 		private bool m_UseStick = true;
 
 		/// <summary> 左スティック </summary>
 		[SerializeField]
 		private InputStickObject m_InputStick = null;
-
-		///// <summary> 右スティック </summary>
-		//[SerializeField]
-		//private InputStickObject m_RightStick = null;
-
-		[SerializeField]
-		private Image  m_Boost = null;
-
-		private void Start()
-		{
-			// ブースト状態の切り替え
-			var trigger = m_Boost.gameObject.AddComponent<ObservableEventTrigger>();
-			trigger.OnPointerDownAsObservable().Subscribe(_ => IsBoost = true).AddTo(this);
-			trigger.OnPointerUpAsObservable().Subscribe(_ => IsBoost = false).AddTo(this);
-		}
 
 		public float GetAxisHorizontal()
 		{
