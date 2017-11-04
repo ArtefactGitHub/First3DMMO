847020485bb548d3306f1ba54e6c0c1e7c97e156
artefact <artefactgo@gmail.com>
2017-11-04 20:03:16 +0900

    通信レスポンスクラスの追加 　・プレイヤー同期データ

---
+++ Assets/WorkSpace/05_SyncPlayer/ResponseDefine.cs
@@ -0,0 +1,22 @@
+﻿using com.Artefact.FrameworkNetwork.Cores;
+using Newtonsoft.Json.Linq;
+
+namespace com.Artefact.First3DMMO.WorkSpace.SyncPlayer
+{
+	public class ResponseSyncPlayer : AResponse
+	{
+		public int PlayerIndex { get; private set; }
+
+		public override void TryParse(JObject obj)
+		{
+			ResponseCode = obj[KeyResponseCode].ToObject<int>();
+
+			PlayerIndex = obj[KeyData]["playerData"]["playerIndex"].ToObject<int>();
+		}
+
+		public override string ToString()
+		{
+			return string.Format("ResponseSyncPlayer\n ResponseCode={0} PlayerIndex={1}", ResponseCode, PlayerIndex);
+		}
+	}
+}
